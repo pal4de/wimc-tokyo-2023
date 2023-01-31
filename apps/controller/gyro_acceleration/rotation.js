8fea44326700fe758e927575cf6bfe4daec1bcb7
RyogaTakao <170441089@ccalumni.meijo-u.ac.jp>
2023-02-01 00:42:02 +0900

    feat:回転数確認機能(仮)追加

---
+++ apps/controller/gyro_acceleration/rotation.js
@@ -0,0 +1,62 @@
+// ジャイロセンサーからコントローラーの向きを判別する。
+import {requestGPIOAccess} from "./node_modules/node-web-gpio/dist/index.js";
+import {requestI2CAccess} from "./node_modules/node-web-i2c/index.js";
+import MPU6050 from "@chirimen/mpu6050";
+
+const sleep = msec => new Promise(resolve => setTimeout(resolve, msec));
+let count = 0; // 半周カウント
+
+async function main() {
+  const gpioAccess = await requestGPIOAccess();
+  const port5 = gpioAccess.ports.get(5);
+  await port5.export("in");
+  let flag = false;  //回転認識用のフラグ
+  let circle;
+  while (true) {
+    var val = await port5.read();
+    if (val == 0) {
+      if (!flag) {
+        flag = true;
+      }
+      checkRotation();
+    } else {
+      if (flag) {
+        flag = false;
+        circle = Math.ceil(count / 2);
+        sendResult(circle);
+        count = 0;
+        circle = 0;
+      }
+    }
+    await sleep(500);
+  }
+}
+
+async function checkRotation() {
+  var i2cAccess = await requestI2CAccess();
+  var port = i2cAccess.ports.get(1);
+  var mpu6050 = new MPU6050(port, 0x68);
+  await mpu6050.init();
+  const data = await mpu6050.readAll();
+  const accel = [data.gx, data.gy, data.gz];
+  const gyro =[data.rx, data.ry, data.rz];
+  let value_judge = 0;  // 周回判断基準値
+  console.log(gyro[2]);
+  if (value_judge == 0) {
+    if (gyro[2] < 0) {
+      count += 1;
+    } else if (gyro[2] > 0) {
+      count += 1;
+    }
+  } else if (0 < value_judge && gyro[2] < 0) {
+    count += 1;
+  } else if (value_judge < 0 && 0 < gyro[2]) {
+    count += 1;
+  }
+}
+
+async function sendResult(result) {
+  console.log(result)
+}
+
+main();
