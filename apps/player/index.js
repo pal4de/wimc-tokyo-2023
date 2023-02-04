cdf8efc66e74df8c7094eef9c8fa0f9217ca8c2b
SeungHyun Cho <c0305sh@naver.com>
2023-02-04 13:50:55 +0900

    Add mokDataからデータを取って音楽が再生できるように

--- apps/player/index.js
+++ apps/player/index.js
@@ -1,11 +1,29 @@
 import player from 'node-wav-player';
 import { PresetCode } from './controller/preset_code.js'
 import { PresetDrum } from './controller/preset_drum.js'
+import { mokRequest } from './request.js';
+import { TEMPO } from './const.js';
 
-const c = new PresetCode(player, "C", 3);
-const Dm = new PresetCode(player, "Dm", 4);
-const DrArrayDm = new PresetDrum(player, 2)
+// const C = new PresetCode(player, "C", 3);
+// const Dm = new PresetCode(player, "Dm", 4);
+// const F = new PresetCode(player, "F", 4);
+// const G = new PresetCode(player, "F", 4);
+// const DrArrayDm = new PresetDrum(player, 1)
 
-await c.play();
-await Dm.play();
-DrArrayDm.play();
+let code;
+let drum;
+
+for(const c of mokRequest.controllers) {
+    code = new PresetCode(player, c.playlist_preset, c.notes);
+    drum = new PresetDrum(player, c.drum_pattern);
+
+
+    await new Promise((resolve) => {
+        setTimeout(() => {
+            console.log('📣 change next controller...')
+            code.play();
+            drum.play();
+            resolve();
+        }, TEMPO * 4);
+    });
+}
--- apps/player/request.js
+++ apps/player/request.js
@@ -1,24 +1,24 @@
-{
+export const mokRequest = {
     "request_id": "1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed",
     "controllers": [
         {
             "bt_address": "34-22-79-74-B1-92",
             "notes": [
-                0,
+                4,
                 1,
-                2,
+                3,
                 4
             ], // 0: 無音, 1: 低音, 2: 中低音, ...
-            "drum_pattern": 1, // 0...2: dramパターン
+            "drum_pattern": 1, // 1, 2: dramパターン
             "playlist_preset": "G7" // "G7", "C", "Dm", "F": コントローラー固有のコード
         },
         {
             "bt_address": "15-EE-B4-96-8D-A9",
             "notes": [
-                3,
                 4,
-                1,
-                2
+                4,
+                2,
+                3
             ],
             "drum_pattern": 2,
             "playlist_preset": "C"
@@ -31,7 +31,7 @@
                 1,
                 2
             ],
-            "drum_pattern": 3,
+            "drum_pattern": 1,
             "playlist_preset": "Dm"
         },
         {
